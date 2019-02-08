f22833a84d0d8f9fbbf2affe947e8275eecbd0df
catallo david <=>
2019-02-08 14:49:16 +0100

    Abonne : save, update, delete and find

--- models/Abonne.php
+++ models/Abonne.php
@@ -15,6 +15,7 @@ class Abonne extends Db {
         $this->setId($id);
     }
 
+
     /**
      * Get the value of id
      */ 
@@ -75,4 +76,111 @@ class Abonne extends Db {
 
         return $this;
     }
+
+    
+    
+    public function save() {
+
+        $data = [
+            "prenom"        => $this->prenom(),
+            "nom"           => $this->nom()
+        ];
+
+        if ($this->id() > 0) { 
+
+            return $this->update();
+
+        }
+
+        $nouvelId = Db::dbCreate(self::TABLE_NAME, $data);
+        $this->setId($nouvelId);
+
+        return $this;
+
+    }
+
+
+    public function update() {
+
+        if ($this->id > 0) {
+
+            $data = [
+                "prenom"        => $this->prenom(),
+                "nom"           => $this->nom(),
+                "id"            => $this->id()
+            ];
+            Db::dbUpdate(self::TABLE_NAME, $data);
+            return $this;
+        }
+        return;
+
+    }
+
+
+    public function delete() {
+
+        $data = [
+            'id' => $this->id(),
+        ];
+        
+        Db::dbDelete(self::TABLE_NAME, $data);
+
+        return;
+    }
+
+
+    public static function findAll($objects = true) {
+
+        $data = Db::dbFind(self::TABLE_NAME);
+        
+        if ($objects) {
+            $objectsList = [];
+            foreach ($data as $d) {
+                $objectsList[] = new Abonne($d['prenom'], $d['nom'], intval($d['id']));
+            }
+            return $objectsList;
+        }
+        return $data;
+    }
+
+
+    public static function find(array $request, $objects = true) {
+
+        $data = Db::dbFind(self::TABLE_NAME, $request);
+
+        if ($objects) {
+            $objectsList = [];
+
+            foreach ($data as $d) {
+                $objectsList[] = new Abonne($d['prenom'], $d['nom'], intval($d['id']));
+            }
+            return $objectsList;
+        }
+        return $data;
+    }
+
+
+    public static function findOne(int $id, $object = true) {
+
+        $request = [
+            ['id', '=', $id]
+        ];
+
+        $data = Db::dbFind(self::TABLE_NAME, $request);
+
+        if (count($data) > 0) {
+            
+            $data = $data[0];
+        
+        }
+
+        else return;
+
+        if ($object) {
+            $abonne = new Abonne($data['prenom'], $data['nom'], intval($data['id']));
+            return $abonne;
+        }
+        return $data;
+    }
+ 
 }
